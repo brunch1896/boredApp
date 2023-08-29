f961336058709a9f7c9aaca57a94e27d9018b985
brunch1896 <brunch_zhu@163.com>
2023-08-29 21:37:58 +0800

    boredApp.py

--- boredApp.py
+++ boredApp.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-st.title('🏀 Bored API app')
+st.title('🏀 双胖周末做什么？')
 
 st.sidebar.header('Input')
 selected_type = st.sidebar.selectbox('Select an activity type', ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"])
@@ -12,19 +12,19 @@ suggested_activity = json_data.json()
 
 c1, c2 = st.columns(2)
 with c1:
-  with st.expander('About this app'):
-    st.write('Are you bored? The **Bored API app** provides suggestions on activities that you can do when you are bored. This app is powered by the Bored API.')
+  with st.expander('双胖是谁'):
+    st.write('很简单，就是乐胖和鸿胖~')
 with c2:
   with st.expander('JSON data'):
     st.write(suggested_activity)
 
-st.header('Suggested activity')
+st.header('建议的活动')
 st.info(suggested_activity['activity'])
 
 col1, col2, col3 = st.columns(3)
 with col1:
-  st.metric(label='Number of Participants', value=suggested_activity['participants'], delta='')
+  st.metric(label='参与人数', value=suggested_activity['participants'], delta='')
 with col2:
-  st.metric(label='Type of Activity', value=suggested_activity['type'].capitalize(), delta='')
+  st.metric(label='活动类型', value=suggested_activity['type'].capitalize(), delta='')
 with col3:
-  st.metric(label='Price', value=suggested_activity['price'], delta='')
+  st.metric(label='价格', value=suggested_activity['price'], delta='')
